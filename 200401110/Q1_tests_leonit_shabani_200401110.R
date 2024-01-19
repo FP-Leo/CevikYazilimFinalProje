71ea7613f677e7e2d46e2e09f27877168d6d65ac
Leonit Shabani <leonitshabani3@gmail.com>
2024-01-19 15:22:09 +0300

    Özellik 3, testleri ile tamamlandı

--- 200401110/Q1_tests_leonit_shabani_200401110.R
+++ 200401110/Q1_tests_leonit_shabani_200401110.R
@@ -1,1 +1,20 @@
 rm(list = ls())
+
+#Q3
+
+# Test 1
+test_that("Global Workspace’de myDF adlı bir değişken var.", {
+  expect_true(exists("myDF"))
+})
+# Test 2
+test_that("myDF Data Frame olamalı.", {
+  expect_true(is.data.frame(myDF))
+})
+# Test 3
+test_that("myDF 18,214 satırlı olmalı", {
+  expect_true(nrow(myDF) == 18214)
+})
+# Test 4
+test_that("myDF 8 sutunlu olmalı", {
+  expect_true(ncol(myDF) == 8)
+})
