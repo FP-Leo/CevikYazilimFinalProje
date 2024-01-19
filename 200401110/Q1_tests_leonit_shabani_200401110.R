5dd22fa1ede023f2508fbc80a6e933503a645ae9
Leonit Shabani <leonitshabani3@gmail.com>
2024-01-19 15:26:47 +0300

    Özellik 5, testleri ile tamamlandı

--- 200401110/Q1_tests_leonit_shabani_200401110.R
+++ 200401110/Q1_tests_leonit_shabani_200401110.R
@@ -18,3 +18,39 @@ test_that("myDF 18,214 satırlı olmalı", {
 test_that("myDF 8 sutunlu olmalı", {
   expect_true(ncol(myDF) == 8)
 })
+
+# Q5
+
+# Test 5
+test_that("Global Workspace’de df_quartet adlı bir değişken var.", {
+  expect_true(exists("df_quartet"))
+})
+# Test 6
+test_that("df_quartet Data Frame olamalı.", {
+  expect_true(is.data.frame(df_quartet))
+})
+# Test 7
+test_that("df_quartet 40 satırlı olmalı", {
+  expect_true(nrow(df_quartet) == 40)
+})
+# Test 8
+test_that("df_quartet 3 sutunlu olmalı", {
+  expect_true(ncol(df_quartet) == 3)
+})
+# Test 9
+test_that("Global Workspace’de df_releases adlı bir değişken var.", {
+  expect_true(exists("df_releases"))
+})
+# Test 10
+test_that("df_releases Data Frame olamalı.", {
+  expect_true(is.data.frame(df_releases))
+})
+# Test 11
+test_that("df_releases 40 satırlı olmalı", {
+  expect_true(nrow(df_releases) == 40)
+})
+
+# Test 12
+test_that("df_releases 3 sutunlu olmalı", {
+  expect_true(ncol(df_releases) == 3)
+})
